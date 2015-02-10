65fd96c03e2035eef4b578fb1b0d31dd38cea3d3
Selçuk Sevinç <selcuk@efabrika.com>
2015-02-10 16:15:12 +0200

    env log

---
+++ boot/env.js
@@ -0,0 +1,21 @@
+module.exports = function(app) {
+
+    var _log = app.system.logger;
+
+    try {
+        _log.info('process env', process.env);
+        _log.info('app name', app.get('name'));
+        _log.info('app env', app.get('env'));
+        _log.info('app port', app.get('port'));
+        _log.info('app basedir', app.get('basedir'));
+    }
+    catch(e) {
+        _log.error(e.stack);
+        return false;
+    }
+
+};
+
+
+
+
