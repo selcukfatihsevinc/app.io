83ec4b09a925593a9134ae9e6b2d2453083c6bd1
Selçuk Sevinç <selcuk@efabrika.com>
2015-05-27 16:59:14 +0300

    v0.0.52
    * model randomise worker
    * randomise library
    * schema query parse function

--- worker/randomize.js
+++ worker/randomize.js
@@ -10,12 +10,13 @@ module.exports = function(app) {
     var _jobs     = app.boot.kue;
     var _mongoose = app.core.mongo.mongoose;
 
-    app.boot.kue.process('denormalize-document', 1, function(job, done) {
+    app.boot.kue.process('randomize-document', 1, function(job, done) {
 
         var params = job.data.params;
         var Model  = _mongoose.model(params.model);
 
         Model.findOne({_id: params.id}, function(err, doc) {
+            doc.r = Math.random();
             doc.save(function(err) {
                 if(err)
                     console.log(err.stack);
