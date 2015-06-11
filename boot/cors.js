d64d5940f5500a326362bf1058a2b7c0b7c6a1c1
Selcuk Fatih Sevinc <airsfs@gmail.com>
2015-06-11 17:35:28 +0300

    v0.0.69
    * boot/cors update

--- boot/cors.js
+++ boot/cors.js
@@ -9,19 +9,6 @@ module.exports = function(app) {
         app.use(cors());
         app.options('*', cors());
 
-        /*
-        app.all('/*', function(req, res, next) {
-            // CORS headers
-            res.header('Access-Control-Allow-Origin', '*'); // restrict it to the required domain
-            res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-
-            // set custom headers for CORS
-            res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,X-HTTP-Method-Override,Content-Type,Accept,X-Client-Id,X-Client-Secret,X-Access-Token');
-
-            next();
-        });
-        */
-
     }
     catch(e) {
         _log.error(e.stack);
