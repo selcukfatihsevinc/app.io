99fc9432e9dac5a46090d48f65efdbd5a6fa5dc5
Selcuk Fatih Sevinc <airsfs@gmail.com>
2015-12-23 17:10:59 +0200

    v0.2.22
    * boot/resize

--- boot/resize.js
+++ boot/resize.js
@@ -1,11 +1,15 @@
-fs = require('fs');
+var mkdirp = require('mkdirp');
+var path   = require('path');
+var mime   = require('mime');
+var fs     = require('fs');
 
 module.exports = function(app) {
 
-    var _log   = app.lib.logger;
-    var _env   = app.get('env');
-    var _group = 'BOOT:RESIZE';
-
+    var _log    = app.lib.logger;
+    var _env    = app.get('env');
+    var _group  = 'BOOT:RESIZE';
+    var _resize = app.config[_env].resize;
+    
     try {
         var target = ['local', 's3', 'cloudinary'];
         var conf   = app.config[_env].app.config.upload;
@@ -43,24 +47,79 @@ module.exports = function(app) {
         function forwards(req, res, next) {
             var url = req.url.split('/_i/');
 
-            if(url.length > 1)
-                req.url = '/'+url[1];
+            if(url.length > 1) {
+                req.url  = '/'+url[1];
+                var size = url[1].split('/');
+                
+                if(size && size.length) {
+                    size = size[0];
+                    
+                    if(_resize) {
+                        if( ! _resize[req.hostname] ) {
+                            _log.error(_group, 'not found hostname');
+                            return res.status(404).end();                            
+                        }
+                        
+                        if( _resize[req.hostname].indexOf(size) == -1 ) {
+                            _log.error(_group, 'not allowed image size');
+                            return res.status(404).end();
+                        }
+                    }
+                }
+            }
 
             next('route');
         }
 
+        function expiresIn(maxAge) {
+            var dt = Date.now();
+            dt += maxAge * 1000;
+
+            return (new Date(dt)).toGMTString();
+        };
+
+        // image expire time
+        var expiry = 60*60*24*90;
+        
         app.get('*', forwards);
         app.get('/*?', function(req, res, next) {
             var image = new Img(req, res);
+            var file  = '/tmp'+req.path;
             
-            if( ! req.tmpcache ) {
-                image.getFile()
-                    .pipe(new streams.identify())
-                    .pipe(new streams.resize())
-                    .pipe(new streams.filter())
-                    .pipe(new streams.optimize())
-                    .pipe(streams.response(req, res));
-            }
+            // check file
+            fs.stat(file, function(err, stat) {
+                if(stat && stat.isFile()) {
+                    _log.info(_group+':FROM:CACHE', req.path);
+                    
+                    fs.readFile(file, function(err, data) {
+                        if( err || ! data )
+                            return res.status(404).end();
+
+                        // set cache
+                        res.set({
+                            'Cache-Control':  'public',
+                            'Expires':        expiresIn(expiry),
+                            'Last-Modified':  (new Date(1000)).toGMTString(),
+                            'Vary':           'Accept-Encoding'
+                        });
+                        
+                        // set type and send response
+                        var lookup = mime.lookup(file);
+                        res.type(lookup || 'text/plain');
+                        res.status(200).send(data);
+                    });
+                }
+                else {
+                    _log.info(_group+':FROM:'+type, req.path);
+                    
+                    image.getFile()
+                        .pipe(new streams.identify())
+                        .pipe(new streams.resize())
+                        .pipe(new streams.filter())
+                        .pipe(new streams.optimize())
+                        .pipe(streams.response(req, res));
+                }
+            });
         });
 
         return true;
